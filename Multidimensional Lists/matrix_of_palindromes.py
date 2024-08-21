a8ee597456c54efba355fec3f2e84f0b568e47e4
lara-c7 <156355249+lara-c7@users.noreply.github.com>
2024-08-21 15:54:45 +0300

    Update matrix_of_palindromes.py

--- Multidimensional Lists/matrix_of_palindromes.py
+++ Multidimensional Lists/matrix_of_palindromes.py
@@ -6,3 +6,12 @@ for row in range(r):
     for col in range(c):
         print(f'{chr(start_letter + row)}{chr(start_letter + row + col)}{chr(start_letter + row)}', end=' ')
     print()
+
+
+# for row in range(r):
+#     data = []
+#     for col in range(c):
+#         data.append(f"{chr(row + 97) + chr(row + col + 97) + chr(row + 97)}")
+#     matrix.append(data)
+#
+# [print(*row) for row in matrix]
